3d951a518050b6a4abb6f1f84dfe6c0ac5ec90f4
jpattWPC <jpatten@westparkcom.net>
2019-10-24 14:04:42 -0500

    syntax errors

--- resources/install/scripts/app/escalations/index.lua
+++ resources/install/scripts/app/escalations/index.lua
@@ -34,7 +34,7 @@ function getmsginfo()
     dbh:query(sql, params, function(rows)
         msgdata['created_epoch'] = rows['created_epoch']
         msgdata['read_epoch'] = rows['read_epoch']
-    )
+    end)
     return msgdata
 end
 
@@ -48,16 +48,17 @@ function getescinfo()
     }
     dbh:query(sql, params, function(rows)
         for row in rows do
-        table.insert(
-            escdata,
-            {
-                voicemail_escalation_order = row['voicemail_escalation_order'],
-                voicemail_escalation_phonenum = row['voicemail_escalation_phonenum'],
-                voicemail_escalation_delay = row['voicemail_escalation_delay']
+            table.insert(
+                escdata,
+                {
+                    voicemail_escalation_order = row['voicemail_escalation_order'],
+                    voicemail_escalation_phonenum = row['voicemail_escalation_phonenum'],
+                    voicemail_escalation_delay = row['voicemail_escalation_delay']
 
-            }
-        )
-    )
+                }
+            )
+        end
+    end)
 end
 
 function originatecall(phonenum)
