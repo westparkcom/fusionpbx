73c22500563903225836698b61fb7edec7c760da
jpattWPC <jpatten@westparkcom.net>
2019-11-11 22:40:47 -0600

    Reduce TTS Overhead

--- resources/install/scripts/app/vmcallout/index.lua
+++ resources/install/scripts/app/vmcallout/index.lua
@@ -2,13 +2,15 @@ ttsvoice = 'Joanna'
 
 api = freeswitch.API();
 vmbox = argv[2]
-introPrompt = "This is the Westpark Communications messaging system. There is a new voicemail in mailbox " .. vmbox:gsub(".", "%1 "):sub(1,-2) .. "."
+introPrompt = "This is the Westpark Communications messaging system. There is a new voicemail in mailbox"
+mboxPrompt = vmbox:gsub(".", "%1 "):sub(1,-2) .. "."
 
 function saytext(textstr)
     return api:executeString('python streamtext voice=' .. ttsvoice .. '|text=' .. textstr);
 end
 
 introPromptWav = saytext(introPrompt)
+mboxPromptWav = saytext(mboxPrompt)
 
 session:setVariable("voicemail_id", vmbox)
 session:setVariable("voicemail_action", "check")
@@ -17,5 +19,7 @@ session:setVariable("voicemail_profile", "default")
 
 session:execute('wait_for_silence', '200 15 10 5000')
 session:execute('playback', introPromptWav)
+session:execute('sleep', '100')
+session:execute('playback', mboxPromptWav)
 session:execute('sleep', '500')
 session:execute('lua', 'app.lua voicemail')
