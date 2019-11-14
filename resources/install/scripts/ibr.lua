9574ed2b90411554c67590105692289c12b390ed
jpattWPC <jpatten@westparkcom.net>
2019-11-14 13:06:13 -0600

    typo

--- resources/install/scripts/ibr.lua
+++ resources/install/scripts/ibr.lua
@@ -4,6 +4,6 @@ if argv[1] == nil then
     freeswitch.consoleLog("ERR", "No UUID set for ibr.lua")
 else
     uuid = argv[1]
-    api:executeString("luarun app ibr-pilots " .. uuid)
+    api:executeString("luarun app.lua ibr-pilots " .. uuid)
     session:execute("endless_playback", "silence_stream://-1")
 end
