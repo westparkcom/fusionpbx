9ca6dbcd33328ebc1a6af17adcefd06265667bc6
jpattWPC <jpatten@westparkcom.net>
2021-08-27 09:43:01 -0500

    Update destinations.php
    typo

--- app/destinations/resources/classes/destinations.php
+++ app/destinations/resources/classes/destinations.php
@@ -299,7 +299,7 @@ if (!class_exists('destinations')) {
 				$select_style = 'width: 400px;';
 				$response = "<link href='https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css' rel='stylesheet' />\n";
 				$response .= "<script src='https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js'></script>\n";
-				$resposne .= "<script>\n";
+				$response .= "<script>\n";
 				$response .= "$(document).ready(function() {\n";
 				$response .= "	$('.formfld').select2();\n";
 				$response .= "});\n";
