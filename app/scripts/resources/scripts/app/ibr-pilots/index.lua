d2d3d2cf21bdec324b785fa2b82a8c762094fc69
jpattWPC <jpatten@westparkcom.net>
2021-09-13 14:58:40 -0500

    Add license info

--- app/scripts/resources/scripts/app/ibr-pilots/index.lua
+++ app/scripts/resources/scripts/app/ibr-pilots/index.lua
@@ -1,3 +1,8 @@
+-- The contents of this file are subject to the Mozilla Public License
+-- Version 1.1 (the "License"); you may not use this file except in
+-- compliance with the License. You may obtain a copy of the License at
+-- https://www.mozilla.org/en-US/MPL/1.1/
+
 -- Queue Recipe Processor
 
 -- The default music on hold stream
