3c1e780ec4b4491265c71c0b38b01cdb31c40cdb
jpattWPC <jpatten@westparkcom.net>
2021-11-10 11:10:26 -0600

    Add greater transfer flexibility

--- app/scripts/resources/scripts/app/ibr-pilots/index.lua
+++ app/scripts/resources/scripts/app/ibr-pilots/index.lua
@@ -660,7 +660,7 @@ end
 function transfer(destdata)
     -- Transfer to FreeSWITCH destination
     local destination = destdata[1]
-    api:executeString("uuid_transfer " .. callinfo["call_uuid"] .. " '" .. destination .. "' inline")
+    api:executeString("uuid_transfer " .. callinfo["call_uuid"] .. " " .. destination)
     bridged = true
     return
 end
